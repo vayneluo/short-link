51bd9c15b753fe15ebadb7b139f792a2a130bf50
vayneluo <vayneluo@haizol.com>
2022-06-27 21:52:19 +0800

    feat: import redis template

---
+++ src/main/java/me/lattice/shortlink/common/config/RedisConfig.java
@@ -0,0 +1,108 @@
+package me.lattice.shortlink.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.time.Duration;
+
+/**
+ * @description: redis config
+ * @author: lattice
+ * @date: 2022/6/27 9:33 PM
+ */
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.database}")
+    private int database;
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.port}")
+    private int port;
+    @Value("${spring.redis.password}")
+    private String password;
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+    @Value("${spring.redis.jedis.pool.max-active}")
+    private int maxActive;
+    @Value("${spring.redis.jedis.pool.max-wait}")
+    private long maxWaitMillis;
+    @Value("${spring.redis.jedis.pool.max-idle}")
+    private int maxIdle;
+    @Value("${spring.redis.jedis.pool.min-idle}")
+    private int minIdle;
+
+    /**
+     * 连接池配置信息
+     */
+
+    @Bean
+    public JedisPoolConfig jedisPoolConfig() {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        // 最大连接数
+        jedisPoolConfig.setMaxTotal(maxActive);
+        // 当池内没有可用连接时，最大等待时间
+        jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
+        // 最大空闲连接数
+        jedisPoolConfig.setMinIdle(maxIdle);
+        // 最小空闲连接数
+        jedisPoolConfig.setMinIdle(minIdle);
+        // 其他属性可以自行添加
+        return jedisPoolConfig;
+    }
+
+    /**
+     * Jedis 连接
+     */
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory(JedisPoolConfig jedisPoolConfig) {
+        JedisClientConfiguration jedisClientConfiguration = JedisClientConfiguration.builder().usePooling()
+                .poolConfig(jedisPoolConfig).and().readTimeout(Duration.ofMillis(timeout)).build();
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
+        return new JedisConnectionFactory(redisStandaloneConfiguration, jedisClientConfiguration);
+    }
+
+    /**
+     * 缓存管理器
+     *
+     * @param connectionFactory
+     * @return
+     */
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        return RedisCacheManager.create(connectionFactory);
+    }
+
+
+    /**
+     * redis template serializer init
+     * @param factory
+     * @return
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory factory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory(jedisPoolConfig()));
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+}
