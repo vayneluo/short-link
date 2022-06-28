cfebaa8f166a24db053bf4d6505cec5befc32639
Vayne.Luo <vayluo@haizol.com>
2022-06-28 16:55:20 +0800

    feat: 短链二维码生成 Done

--- src/main/java/me/lattice/shortlink/web/controller/ShortLinkController.java
+++ src/main/java/me/lattice/shortlink/web/controller/ShortLinkController.java
@@ -1,14 +1,22 @@
 package me.lattice.shortlink.web.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import me.lattice.shortlink.annotation.SimpleController;
 import me.lattice.shortlink.common.result.Result;
+import me.lattice.shortlink.common.utiils.QRCodeUtil;
 import me.lattice.shortlink.service.ShortLinkService;
 import me.lattice.shortlink.web.req.ShortLinkGenReq;
 import me.lattice.shortlink.web.rsp.ShortLinkGenRsp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @description: 短链生成器
@@ -22,9 +30,32 @@ public class ShortLinkController {
     @Autowired
     ShortLinkService shortLinkService;
 
+    @ApiOperation(value = "短链生成")
     @PostMapping(value = "/link/generate")
     public Result<ShortLinkGenRsp> generateShortLink(@RequestBody ShortLinkGenReq req) {
         return new Result<>(shortLinkService.generateShortLink(req));
+    }
+
+    @ApiOperation(value = "短链二维码生成")
+    @RequestMapping(value = "/link/qrcode")
+    public void qrcode(String content, @RequestParam(defaultValue = "300", required = false) int width
+            , @RequestParam(defaultValue = "300", required = false) int height
+            , HttpServletResponse response) {
+        ServletOutputStream outputStream = null;
+        try {
+            outputStream = response.getOutputStream();
+            QRCodeUtil.writeToStream(content, outputStream, width, height);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
 }
